f61f6dab698a41a67c7620b51541e670222eef20
LakkamaneniVarshini <lakkamanenivarshini@gmail.com>
2024-06-11 11:07:53 +0530

    database connection

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link";
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 
 export default function LoginForm(){
     const[email,setEmail]=useState("")
