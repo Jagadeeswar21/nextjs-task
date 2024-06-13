5137e163bf1e37eb3401098d317932bdec67e807
Jagadeeswar21 <jagadeeswarmarella21@gmail.com>
2024-06-13 11:08:51 +0530

    added redirection based on role

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -14,43 +14,51 @@ export default function LoginForm() {
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        try{
-            const res=await signIn("credentials",{
-                email,password,redirect:false,});
-                if(res?.error){
-                    setError("invalid credentials")
-                    return;
-                }
-                router.push("/dashboard")
-            }catch(error){
-                console.log(error)
-            }           
+        try {
+            const res = await signIn("credentials", {
+                email,
+                password,
+                redirect: false,
+            });
+
+            if (res?.error) {
+                setError("Invalid credentials");
+                return;
+            }
+            if (email.includes("@admin")) {
+                router.push("/adminDashboard");
+            } else {
+                router.push("/dashboard"); 
+            }
+        } catch (error) {
+            console.log(error);
+            setError("An error occurred. Please try again.");
+        }
     };
+
     return (
-        <div className="grid place-items-center h-screen"> 
+        <div className="grid place-items-center h-screen">
             <div className="shadow-lg p-6 rounded-lg border-t-4 bg-orange-200">
                 <h1 className="text-xl font-bold my-4">Login Page</h1>
                 <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-                    <input 
-                        type="text" 
-                        placeholder="Email" 
+                    <input
+                        type="text"
+                        placeholder="Email"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
-                    <input  
-                        type="password" 
+                    <input
+                        type="password"
                         placeholder="Password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                     <button className="bg-orange-400 text-white font-bold px-6 py-2">Login</button>
-
                     {error && (
-                        <div className="text-red w-fit text-sm py-1 px-3 rounded-md mt-2">
+                        <div className="text-red-500 w-fit text-sm py-1 px-3 rounded-md mt-2">
                             {error}
                         </div>
                     )}
-
                     <Link className="text-sm mt-3 text-right" href="/register">
                         Don t have an account? <span className="underline">Register</span>
                     </Link>
