94558e0d4417611e54d4dd2a838e397839407e9f
LakkamaneniVarshini <lakkamanenivarshini@gmail.com>
2024-06-13 09:35:01 +0530

    changed schema

--- models/schema.ts
+++ models/schema.ts
@@ -17,7 +17,7 @@ const userSchema=new Schema(
         status:{
             type:String,
             enum: ["active", "inactive"],
-            default: "inactive",
+            default: "active",
             required: true,
         },
         modifiedAt: {
