82b65b4147a10b5b9fba41850d1ad61c9e065195
LakkamaneniVarshini <lakkamanenivarshini@gmail.com>
2024-06-14 17:17:16 +0530

    leaves

---
+++ src/app/components/leavePage.tsx
@@ -0,0 +1,5 @@
+export default function LeavePage(){
+    return (
+        <div> hi</div>
+    )
+}
--- src/app/dashboard/leaves/page.tsx
+++ src/app/dashboard/leaves/page.tsx
@@ -1,7 +1,8 @@
+import LeavePage from "@/app/components/leavePage"
 export default function Leaves(){
     return(
         <div className="text-2xl text-center">
-            Leave Page 
+            <LeavePage/> 
         </div>
     )
     
