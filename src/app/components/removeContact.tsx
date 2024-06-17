c908b6d906e7e5f6c023296d64b688e15857a86e
L.Varshini <lakkamanenivarshini@gmail.com>
2024-06-17 10:24:56 +0530

    remove contact

---
+++ src/app/components/removeContact.tsx
@@ -0,0 +1,32 @@
+// components/RemoveContact.tsx
+import React from 'react';
+import { HiTrash } from 'react-icons/hi';
+import { useRouter } from 'next/navigation';
+interface RemoveContactProps {
+  id: string;
+  onDelete:()=>void;
+}
+
+const RemoveContact: React.FC<RemoveContactProps> = ({ id,onDelete}) => {
+  const handleDelete = async () => {
+    try {
+      const res = await fetch(`/api/contacts/${id}`, { method: 'DELETE' });
+      if (res.ok) {
+        onDelete()
+        
+      } else {
+        console.error('Failed to delete contact');
+      }
+    } catch (error) {
+      console.error('Failed to delete contact', error);
+    }
+  };
+
+  return (
+    <button onClick={handleDelete} className="text-red-500 hover:text-red-700 p-2">
+      <HiTrash />
+    </button>
+  );
+};
+
+export default RemoveContact;
