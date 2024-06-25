ba6c390b5bb1fb600c0132685cabad4d991c6cbd
Jagadeeswar21 <jagadeeswarmarella21@gmail.com>
2024-06-25 17:02:06 +0530

    scrolling changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <Header />
       <div className="flex flex-1">
         <Sidebar/>
-        <main className="flex-1 p-4">{children}</main>
+        <main className="flex-1 p-4 ml-[10rem] mt-[64px] h-[calc(100vh-128px)] overflow-y-auto w-[calc(100%-10rem)]">{children}</main>
       </div>
       <Footer />
     </div>
