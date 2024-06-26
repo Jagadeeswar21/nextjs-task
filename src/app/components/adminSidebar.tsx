cfdb937a84c587c9c7f75202fb5f2018c5a43c6f
Jagadeeswar21 <jagadeeswarmarella21@gmail.com>
2024-06-26 16:49:53 +0530

    admi ui changes

--- src/app/components/adminSidebar.tsx
+++ src/app/components/adminSidebar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Adminsidebar: React.FC = () => {
   return (
-    <aside className="w-64 bg-gray-700 text-white flex-shrink-0">
+    <aside className="w-[10rem] fixed left-0 h-[calc(100vh-128px)] mt-[64px] bg-gray-700 text-white flex-shrink-0">
       <nav className="flex flex-col p-4">
         <Link href="/adminDashboard/users"
            className="py-2 px-4 hover:bg-gray-700 rounded">Users
