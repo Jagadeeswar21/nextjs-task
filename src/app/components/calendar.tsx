e5c26998a1edfeb8b8466ab4b703231cc3c41938
L.Varshini <lakkamanenivarshini@gmail.com>
2024-07-22 13:56:03 +0530

    calendar

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -1,16 +1,16 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { Calendar, momentLocalizer}  from 'react-big-calendar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { useSession } from 'next-auth/react';
 import toast from 'react-hot-toast';
 
-// Setup the localizer for react-big-calendar
 const localizer = momentLocalizer(moment);
 
 type LeaveData = {
-  date: string;
+  startDate: string;
+  endDate: string;
   user: {
     name: string;
   };
@@ -18,7 +18,6 @@ type LeaveData = {
 
 const LeaveCalendar: React.FC = () => {
   const { data: session } = useSession();
-  const [leaveData, setLeaveData] = useState<LeaveData[]>([]);
   const [events, setEvents] = useState<any[]>([]);
 
   useEffect(() => {
@@ -30,15 +29,23 @@ const LeaveCalendar: React.FC = () => {
             throw new Error('Failed to fetch leave data');
           }
           const data = await response.json();
-          setLeaveData(data.leaves);
+          const formattedEvents: any[] = [];
+          data.leaves.forEach((leave: LeaveData) => {
+            const start = new Date(leave.startDate);
+            const end = new Date(leave.endDate);
+            let currentDate = start;
+            
+            while (currentDate <= end) {
+              formattedEvents.push({
+                start: new Date(currentDate),
+                end: new Date(currentDate),
+                title: leave.user?.name,
+                allDay: true,
+              });
+              currentDate.setDate(currentDate.getDate() + 1);
+            }
+          });
           
-          // Convert leave data to events format
-          const formattedEvents = data.leaves.map((leave: LeaveData) => ({
-            start: new Date(leave.date),
-            end: new Date(leave.date),
-            title: leave.user?.name,
-            allDay: true,
-          }));
           setEvents(formattedEvents);
         } catch (error) {
           console.error('Error fetching leave data:', error);
@@ -52,13 +59,15 @@ const LeaveCalendar: React.FC = () => {
   }, [session]);
 
   return (
-    <div className="calendar-container">
+    <div className="calendar-container p-10">
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         style={{ height: '100%', width: '100%' }}
+        views={['month']}
+        popup
       />
     </div>
   );
