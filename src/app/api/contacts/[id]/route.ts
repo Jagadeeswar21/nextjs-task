98e1aa3ec580219d8e7c70b4ce9efda8f0408625
L.Varshini <lakkamanenivarshini@gmail.com>
2024-08-05 11:43:43 +0530

    changes

--- src/app/api/contacts/[id]/route.ts
+++ src/app/api/contacts/[id]/route.ts
@@ -1,37 +1,52 @@
 import { connectMongoDB } from "../../../../../lib/mongodb";
 import Contact from "../../../../../models/contactSchema";
 import { NextResponse} from "next/server";
-
+import SharedContact from "../../../../../models/sharedContactsSchema";
 type Params = {
   id: string;
 };
+export async function PUT(req: Request, { params }: { params: Params }) {
+  const { id } = params;
+  const { name, email, phone, status } = await req.json();
+  
+  await connectMongoDB();
 
-export async function PUT(req:Request,{ params }: { params: Params }) {
-    const { id } = params;
-    try {
-    const { name, email, phone, status } =await req.json();
-    console.log(name)
-      await connectMongoDB();
-      const updatedContact = await Contact.findByIdAndUpdate(id, { name, email, phone, status }, { new: true });
-      console.log(updatedContact)
-      return NextResponse.json(updatedContact,{status:200});
-    } catch (error) {
-      return NextResponse.json({ message: 'Failed to update contact' },{status:500});
+  try {
+    let updatedContact = await Contact.findByIdAndUpdate(id, { name, email, phone, status }, { new: true });
+    if (!updatedContact) {
+      const sharedContact = await SharedContact.findById(id).populate('contact');
+      if (sharedContact) {
+        updatedContact = await Contact.findByIdAndUpdate(sharedContact.contact._id, { name, email, phone, status }, { new: true });
+      }
     }
+
+    if (updatedContact) {
+      return NextResponse.json(updatedContact, { status: 200 });
+    } else {
+      return NextResponse.json({ message: 'Contact not found' }, { status: 404 });
+    }
+  } catch (error) {
+    return NextResponse.json({ message: 'Failed to update contact', error }, { status: 500 });
   }
+}
 
 export async function DELETE(req: Request, { params }: { params: Params }) {
   const { id } = params;
-  console.log(id)
+
   await connectMongoDB();
-      try {
-        const deletedContact = await Contact.findByIdAndDelete(id);
-        console.log(deletedContact)
-        if (!deletedContact) {
-          return NextResponse.json({ message: 'Contact not found' },{status:404});
-        }
-        return NextResponse.json({ message: 'Contact deleted successfully' },{status:200});
-      } catch (error) {
-        return NextResponse.json({ message: 'Failed to delete contact', error });
+  try {
+    let deletedContact = await Contact.findByIdAndDelete(id);
+    if (!deletedContact) {
+      const sharedContact = await SharedContact.findByIdAndDelete(id);
+      if (sharedContact) {
+        return NextResponse.json({ message: 'Shared contact deleted successfully' }, { status: 200 });
+      } else {
+        return NextResponse.json({ message: 'Contact not found' }, { status: 404 });
       }
+    } else {
+      return NextResponse.json({ message: 'Contact deleted successfully' }, { status: 200 });
+    }
+  } catch (error) {
+    return NextResponse.json({ message: 'Failed to delete contact', error }, { status: 500 });
+  }
 }
